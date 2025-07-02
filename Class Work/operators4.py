6d375b7847b963a23d3dc63d70feb06cda615ed1
Gopi krishna <gopikrishna6547@gmail.com>
2025-07-02 06:17:06 +0000

    operators.4

--- Class Work/operators4.py
+++ Class Work/operators4.py
@@ -8,3 +8,11 @@ print("Float division(//):",a//b)#Float division(//): 2
 print("Modulus(%):",a%b)#Modulus(%): 0
 print("Modulus(%):",b%a)#Modulus(%): 10
 print("Exponentiation(**):",a**b)#Exponentiation(**): 10240000000000
+
+#2.COMPARISION OPERATORS
+print("Equal to(==):",a==b)#Equal to(==): False
+print("Not Equal to(!=);",a!=b)
+print("Greater than(>):",a>b)
+print("Less than(<):",a<b)
+print("Greater than or Equal to(>=):",a>=b)#
+print("Lesser than or Equal to(<=):",a<=b)#
