b6f35081a4b9ef2d266baf9eabb4f4b1025dc8a9
Gopi krishna <gopikrishna6547@gmail.com>
2025-07-02 06:01:38 +0000

    operators4

--- Class Work/operators4.py
+++ Class Work/operators4.py
@@ -1,10 +1,10 @@
 a=20
 b=10
-print("Addition(+):",a+b)
-print("Subtraction(-):",a-b)
-print("Multiplication(*):",a*b)
-print("Division(/):",a/b)
-print("Float division(//):",a//b)
-print("Modulus(%):",a%b)
-print("Modulus(%):",b%a)
-print("Exponentiation(**):",a**b)
+print("Addition(+):",a+b)#Addition(+): 30
+print("Subtraction(-):",a-b)#Subtraction(-): 10
+print("Multiplication(*):",a*b)#Multiplication(*): 200
+print("Division(/):",a/b)#Division(/): 2.0
+print("Float division(//):",a//b)#Float division(//): 2
+print("Modulus(%):",a%b)#Modulus(%): 0
+print("Modulus(%):",b%a)#Modulus(%): 10
+print("Exponentiation(**):",a**b)#Exponentiation(**): 10240000000000
