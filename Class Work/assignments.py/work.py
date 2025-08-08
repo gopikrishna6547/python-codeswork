76eef08b7845aa5564fc3bc32bcb092d38e077c7
Your Name <you@example.com>
2025-08-08 15:13:22 +0530

    work.py

--- Class Work/assignments.py/work.py
+++ Class Work/assignments.py/work.py
@@ -2,3 +2,19 @@
 list=[10,20,30,40,50,60,70,80,90]
 total=sum(list)
 print(total)
+#write a python program to  multiple all numbers in list?
+my_list = [1, 2, 3, 4, 5]
+result = 1
+for num in my_list:
+    result *= num
+print("Product:", result)
+#find smallest number in list?
+my_list = [10, 20, 4, 45, 99]
+print("Smallest number:", min(my_list))
+# Find largest number in a list?
+my_list = [10, 20, 4, 45, 99]
+print("Largest number:", max(my_list))
+# Find second largest number in a list
+my_list = [10, 20, 4, 45, 99]
+my_list.sort()
+print("Second largest number:", my_list[-2])
